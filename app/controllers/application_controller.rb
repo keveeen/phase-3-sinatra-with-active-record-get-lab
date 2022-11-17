6efcd5bfed1f0c856a392ce2a2483fa041cad008
keveeen <105822487+keveeen@users.noreply.github.com>
2022-11-17 18:30:02 +0100

    commit tree 8d84bfe1b0a5b44acea3b62cf0eed1a7b3e510a1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,44 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
 
   # add routes
+  set :default_content_type, 'application/json'
+  
+
+  get '/bakeries' do
+
+    
+
+    bakeries = Bakery.all
+
+    bakeries.to_json
+    
+  end
+  
+  get '/bakeries/:id' do
+
+    bakery = Bakery.find(params[:id])
+
+    bakery.to_json(include: :baked_goods)
+
+  end
+
+  get '/baked_goods/by_price' do
+
+    goods = BakedGood.all
+
+    goods.order(price: :desc).to_json
+  
+  end
+
+  get '/baked_goods/most_expensive' do
+    goods = BakedGood.all
+    sorted = goods.order(price: :desc).limit(1)
+    # binding.pry
+    sorted[0].to_json
+
+  end
+
 
 end
